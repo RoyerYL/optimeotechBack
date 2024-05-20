7274bd864302b48e970fd8d55cae666080aeae4f
RoyerFrba <royeryl03@gmail.com>
2024-05-20 15:16:09 -0300

    Backend

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -4,11 +4,11 @@ const { Op, fn, col } = require('sequelize');
 const { Category } = require('../db');
 const createCategory = async (name) => {
     try {
-        const [category, created] = await Category.findOrCreate({
-            where: { name },
-            defaults: { name }
-        });
-        return { category, created };
+        const category = await Category.create({
+            name: name
+            // no incluyas 'id' aquí
+          });
+        return category ;
     } catch (error) {
         throw new Error('Error creating user: ' + error.message);
     }
