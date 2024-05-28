e53c2f0db266198e0519942fedcb9c5b29190bfe
RoyerYampasi <royeryl03@gmail.com>
2024-05-27 22:23:37 -0300

    comit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { conn } = require('./src/db.js');
 const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: false }).then(() => {
     server.listen(PORT, () => {
       console.log(`Listening at ${PORT}`); // eslint-disable-line no-console
     });
