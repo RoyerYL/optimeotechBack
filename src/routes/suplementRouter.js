3e9340adc316f9d1e6da657c96868d02fed9d219
RoyerYampasi <royeryl03@gmail.com>
2024-05-27 22:39:51 -0300

    U

--- src/routes/suplementRouter.js
+++ src/routes/suplementRouter.js
@@ -1,5 +1,5 @@
 const {Router} = require("express");
-const upload = require("../Middleware/upload"); // Importa el middleware de Multer
+const upload = require("../Middleware/Upload"); // Importa el middleware de Multer
 
 const suplementRouter = Router();
 
