6719b6862f1938372e46fb8f9c23dc01625dc5f9
RoyerYampasi <royeryl03@gmail.com>
2024-06-06 18:36:03 -0300

    DEPLOY FIX

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -11,7 +11,6 @@ const client = new MercadoPagoConfig({ accessToken: `${ACCESS_TOKEN}` });
 
 
 const createOrder = async (req, res) => {
-    // console.log(req.body);s
     // const { items, total } = req.body;
 
     try {
@@ -53,7 +52,6 @@ const createOrder = async (req, res) => {
         const preference = new Preference(client)
 
         const result = await preference.create({ body })
-        // console.log(result);
         res.json({ point: result.init_point, });
     } catch (error) {
         console.log(error);
@@ -64,7 +62,6 @@ const createOrder = async (req, res) => {
 }
 
 const receiveWebhook = async (req, res) => {
-    // console.log('datos recibidos en el webhook:', req.query);
 
     const paymentId = req.query['data.id'];
     const topic = req.query.type;
@@ -78,14 +75,11 @@ const receiveWebhook = async (req, res) => {
             });
 
             const payment = response.data;
-            // console.log('Datos de la api de mp:', payment);
 
             const { transaction_details, additional_info, status: mpStatus, payer, metadata } = payment;
             const total_paid_amount = Math.round(transaction_details.total_paid_amount * 100); // convirtiendo a num entero
             const items = additional_info.items;
             const userId = metadata ? metadata.user_id : null;
-            console.log('UserId recibido de metadata:', userId);
-            // console.log('Payer:', payer);
 
             let status;
             if (mpStatus === 'approved') {
@@ -103,7 +97,6 @@ const receiveWebhook = async (req, res) => {
                 userId,
             });
 
-            console.log('Orden creada:', orden);
             // AÑADIR suplementos a la orden
             for (const item of items) {
                 if (!item.title || !item.quantity || !item.unit_price) {
@@ -114,16 +107,16 @@ const receiveWebhook = async (req, res) => {
                 const cantidad = parseInt(item.quantity, 10);
                 const precio = Math.round(parseFloat(item.unit_price) * 100); 
 
-                // console.log(`Buscando suplemento con título: ${suplementoName}`);
 
                 const suplemento = await Suplement.findOne({ where: { name: suplementoName } });
                 if (!suplemento) {
                     throw new Error(`Suplemento con título ${suplementoName} no encontrado en la base de datos`);
                 }
-                // console.log(`Suplemento encontrado: ${suplemento.id}, añadiendo a la orden`);
                 await orden.addSuplement(suplemento, { through: { cantidad, precio } });
 
                 if (status === 'completed') {
+                    console.log(cantidad+"CANTIDAD BACKEND");
+                    console.log(suplemento.amount+"AMOUNT BACKEND");
                     // Reducir el stock permanentemente
                     if (suplemento.amount < cantidad) {
                         console.error(`No hay suficiente stock para el suplemento ${suplemento.name}`);
@@ -131,7 +124,6 @@ const receiveWebhook = async (req, res) => {
                     }
                     suplemento.amount -= cantidad;
                     await suplemento.save();
-                    console.log(`Stock actualizado para el suplemento ${suplemento.name}. Nueva cantidad: ${suplemento.amount}`);
                 }
                 
             }
