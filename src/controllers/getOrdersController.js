bf1ec032b0751a92be9240a1de8278e5876d58d2
RoyerYampasi <royeryl03@gmail.com>
2024-06-07 15:53:20 -0300

    deploy pagionados

--- src/controllers/getOrdersController.js
+++ src/controllers/getOrdersController.js
@@ -1,9 +1,56 @@
+const { Op } = require('sequelize');
 const { Orden, OrdenSuplement, Suplement, User } = require('../db')
 
 const getOrdersController = async (req, res) => {
+    const { fecha, orderBy, orderDirection, page = 1, pageSize = 7 } = req.query;
+    let order = [];
+    if (orderBy && orderDirection) {
+        order = [[orderBy, orderDirection]]
+    }
+    let where = {};
+
+    
+    if (fecha) {
+        const startDate = new Date(fecha);
+        const endDate = new Date(fecha);
+        endDate.setDate(endDate.getDate() + 1);
+
+        where = {
+            ...where,
+            createdAt: {
+                [Op.between]: [startDate, endDate]
+            }
+        };
+    }
+
     try {
-        const orders = await Orden.findAll();
-        res.json(orders);
+        let include = [];
+
+        const offset = (page - 1) * pageSize;
+
+        const body = {
+            include,
+            where,
+            order,
+            limit: pageSize,
+            offset
+        }
+        
+        // Realizar la consulta con Sequelize
+        const { count, rows } = await Orden.findAndCountAll(body);
+        // Calcular el número total de páginas
+        const totalPages = Math.ceil(count / pageSize);
+        
+        // Devolver los suplementos filtrados, el número total de páginas y la página actual
+        
+        res.json({
+            totalPages,
+            currentPage: page,
+            pageSize,
+            totalItems: count,
+            items: rows
+        });
+
     } catch (error) {
         console.error('Error al obtener las ordenes:', error);
         res.status(500).json({ error: 'Error al obtener las ordenes' });
--- src/models/SuplementsModel.js
+++ src/models/SuplementsModel.js
@@ -28,6 +28,10 @@ module.exports = (sequelize) => {
             type: DataTypes.INTEGER,
             allowNull: false
         },
+        sales:{
+            type:DataTypes.INTEGER,
+            defaultValue:0
+        },
         wigth:{
             type:DataTypes.INTEGER,
             defaultValue:1,
